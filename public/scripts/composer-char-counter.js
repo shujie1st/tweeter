be4a1a4e563e72a378f51e8945808b4ddffc6f5f
Shujie Ma <shujie1st@gmail.com>
2023-03-20 18:13:35 -0400

    Add container for the tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,20 @@ $(document).ready(function() {
   $("#tweet-text").on("input", function() {
     let count = 140 - $(this).val().length;
     let output = $(this).parent().find("output.counter");
+
+    // Add class "red" to counter if users exceed the 140 character limit
+    // Or if not exceed limit, remove the class "red" if it already existed
     if (count < 0) {
       if (!output.hasClass("red")) {
         output.addClass("red");
-      }   
+      }
     } else {
-      if (output.hasClass("red")){
+      if (output.hasClass("red")) {
         output.removeClass("red");
       }
     }
+
+    // Update the output value for counter
     output.val(count);
   });
 });
